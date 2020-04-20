7c43843fc66f43df31c1bbadb480edea610915c9
agfebrian <febriananggi9@gmail.com>
2020-04-20 21:37:12 +0700

    memperbaiki tampilan

--- js/script.js
+++ js/script.js
@@ -3,7 +3,15 @@ $.get('https://raw.githubusercontent.com/agfebrian/order-form/master/pizza.json'
 	// console.log(menu);
 
 	$.each(JSON.parse(menu), function (index, item) {
-		$('#product').append(""+ item.nama +"");
+		$('#product').append(
+			`<div class='card-product'>
+				<div>
+					<img src='img/menu/${item.gambar}' style='width: 250px'>
+				</div>
+				<div>
+					<h2>${item.nama}</h2>
+				</div>
+			</div>`);
 		// console.log(item.nama);
 	})
 })
