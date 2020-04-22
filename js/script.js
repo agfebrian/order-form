1b57f2c1f1321bab9841d0686cbedb42cd2ff6f2
agfebrian <febriananggi9@gmail.com>
2020-04-22 11:06:07 +0700

    menambahkan fungsi klik pada setiap produk

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 $.get('https://raw.githubusercontent.com/agfebrian/order-form/master/pizza.json', function (data) {
 	let menu = data;
 	// console.log(menu);
-
 	$.each(JSON.parse(menu), function (index, item) {
 		$('#product').append(`
 			<div class='card-product'>
@@ -19,5 +18,24 @@ $.get('https://raw.githubusercontent.com/agfebrian/order-form/master/pizza.json'
 			</div>
 		`);
 		// console.log(item.nama);
-	})
-})
+	});
+
+	$('.card-product').click(function (event) {
+		var target = $(event.target).parent();
+		var parent = target.parent();
+		var kids = parent.children('.card-body');
+		
+		if( kids.children('h2').html() != undefined ) {
+			// console.log(kids.children('h2').text());
+			var selection = kids.children('h2').text();
+			// console.log(selection);
+			$.get('https://raw.githubusercontent.com/agfebrian/order-form/master/pizza.json', function (data) {
+				$.each(JSON.parse(data), function (index, data) {
+					if( data.nama == selection ) {
+						console.log(data);
+					}
+				});
+			});
+		}
+	});
+});
